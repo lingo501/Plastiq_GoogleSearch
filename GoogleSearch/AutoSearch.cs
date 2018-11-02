5678e292b9da45d3d6fc286e19995ad245c613b5
lingo501 <44685410+lingo501@users.noreply.github.com>
2018-11-02 11:33:22 -0700

    Automate Google Search using Chrome browser

---
+++ GoogleSearch/AutoSearch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.Windows;
+using System.Diagnostics;
+
+namespace GoogleSearch
+{
+    [TestClass]
+    public class AutoSearch
+    {
+        IWebDriver cDriver;
+
+        [TestMethod]
+        public void ChromeSearch()
+        {
+            //Create an instance for IWebDriver and get the instance for Chrome Driver
+            cDriver = new ChromeDriver();
+
+            //Nevigate to Google page
+            cDriver.Navigate().GoToUrl("http://www.google.com");
+
+            //Find search textbox using UI Element
+            IWebElement searchBox = cDriver.FindElement(By.Name("q"));
+
+            //Enter search text
+            searchBox.SendKeys("test automation is awesome");
+
+            //Perform enter Key
+            searchBox.SendKeys(Keys.Return);
+
+            //Get the title of the search page
+            string title = cDriver.Title;
+
+            // print out the search page title in console
+            Console.WriteLine("Title of the search page: {0}", title);
+                        
+            //Close browser
+            cDriver.Close();
+        }
+    }
+}
---
+++ GoogleSearch/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GoogleSearch")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GoogleSearch")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3ddf991f-10ed-4bea-97da-829d5f4c695d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
